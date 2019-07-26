1ac6c6772527239d8a2ab3e03f784fb2b8bc618b
Mark Galante <mark.galante.14@gmail.com>
2019-07-26 16:51:21 +0200

    working on WorkItem.js

--- myportfolio/src/Components/About.js
+++ myportfolio/src/Components/About.js
@@ -4,8 +4,11 @@ const About = props =>{
     return(
         <section>
             <h2>About</h2>
+            <div>
+                {props.aboutData}
+            </div>
         </section>
     )
-}
+}; 
 
 export default About; 
---
+++ myportfolio/src/Components/WorkItem.js
@@ -0,0 +1,29 @@
+import React from 'react'; 
+import moment from 'moment'; 
+
+const WorkItem = props => {
+    const getWorkDates = () => {
+        const startdate = moment(props.workItemData.started).format('MMM, YYYY'); 
+        let enddate = null; 
+        if(props.WorkItemData.ended !== ''){
+            enddate = moment(props.workItem.ended).format('MMM, YYYY');
+        }
+        else{
+            enddate = "Present"; 
+        }
+        return <span> {startdate} - {enddate} </span>
+    }
+
+    const getHighlights = props.WorkItemData.highlights.map(function(item, index){
+        return (<li key={index}>{item}</li>)
+    }); 
+    return(
+        <div>
+            <h3>{props.WorkItem.position}, <span>{props.WorkItem.employer}</span></h3>
+            <p>{getWorkDates()}</p>
+            <p>{props.workItemData.myrole}</p>
+        </div>
+    )
+
+
+}
