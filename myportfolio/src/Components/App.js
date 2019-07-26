68d75f18bdade0e682726fc0b4cdcd3d1ce6ac34
Mark Galante <mark.galante.14@gmail.com>
2019-07-26 08:21:43 +0200

    Created components to add to App.js

--- myportfolio/src/Components/App.js
+++ myportfolio/src/Components/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './App.css';
+import About from './About'; 
+import Education from './Education';
+import Work from './Work'; 
+import Skills from './Skills'; 
 
 const App = props => { 
   const profileData = props.jsonObj.basics; 
@@ -7,6 +11,16 @@ const App = props => {
   const workData = props.jsonObj.work; 
   const skillsData = props.jsonObj.skills; 
   const educationData = props.jsonObj.education; 
+
+  return(
+    <div>
+      <About /> 
+      <Work /> 
+      <Skills /> 
+      <Education /> 
+    </div>
+  )
+
 }
 
 export default App;
---
+++ myportfolio/src/Components/Education.js
@@ -0,0 +1,9 @@
+import React from 'react'; 
+
+const Education = props =>{
+    return(
+        <p>Test: Education</p>
+    )
+}
+
+export default Education; 
