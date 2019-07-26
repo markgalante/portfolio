d636994a75a4f59d4081c6cb9e01212195ce86b4
Mark Galante <mark.galante.14@gmail.com>
2019-07-26 17:10:26 +0200

    Changed skills.skills to skills.keywords

--- myportfolio/src/Components/Skills.js
+++ myportfolio/src/Components/Skills.js
@@ -1,9 +1,9 @@
 import React from 'react'; 
 
 const Skills = props =>{
+    const getSkills = props.skillsData[0].keywords
     return(
         <p>Test: Skills goes here</p>
     )
 }
-
 export default Skills; 
