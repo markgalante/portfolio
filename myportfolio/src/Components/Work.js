91dffcddeefca500041e5b2a3f6fdd55d8bf2f2d
Mark Galante <mark.galante.14@gmail.com>
2019-07-26 17:01:56 +0200

    Added information to Work.js

--- myportfolio/src/Components/Work.js
+++ myportfolio/src/Components/Work.js
@@ -1,9 +1,19 @@
 import React from 'react'; 
+import WorkItem from './WorkItem'; 
 
 const Work = props => {
+    const getWorkExperience = () => {
+        const workItems = [];
+        props.workData.forEach((val, index) =>{
+            workItems.push(<WorkItem key={index} workItemData={val}/>)
+        }) 
+        return workItems; 
+    }
     return(
-        <p>Work Section</p>
-    )
-}
-
+        <section>
+            <h2><i></i>Work Experience</h2>
+            {getWorkExperience()}
+        </section>
+    ); 
+}; 
 export default Work; 
