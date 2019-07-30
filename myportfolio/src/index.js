16247982488ea72961eb0e2c98c508164fb346c5
Mark Galante <mark.galante.14@gmail.com>
2019-07-30 21:36:25 +0200

    Removed json.parse

--- myportfolio/src/index.js
+++ myportfolio/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App'; 
 import * as serviceWorker from './serviceWorker';
-const json = require(JSON.parse('./resume.json'));
+const json = require('./resume.json');
 
 ReactDOM.render(<App jsonObj={json}/>, document.getElementById('root'));
 
