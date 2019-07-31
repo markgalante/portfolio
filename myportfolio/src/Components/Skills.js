7521ce4ea871872af0e5f3ecc7ba81aab9bcf3e3
Mark Galante <mark.galante.14@gmail.com>
2019-07-31 18:24:38 +0200

    Added classNames to Skills.js

--- myportfolio/src/Components/Skills.js
+++ myportfolio/src/Components/Skills.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Skills = props =>{
     const getSkills = props.skillsData.keywords.map(function (item, index){
-        return (<li key={index}><span>{item}</span></li>)
+        return (<li key={index}><span className="label label-success">{item}</span></li>)
     });
 
     return(
-        <section>
-            <h2><i></i>Skills</h2>
-            <ul>{getSkills}</ul>
+        <section className="skills">
+            <h2 className="text-uppercase"><i className="fa fa-lg fa-code"></i>Skills</h2>
+            <ul className="skills-list list-inline">{getSkills}</ul>
         </section>
-    )
+    ) 
 };
 export default Skills; 
