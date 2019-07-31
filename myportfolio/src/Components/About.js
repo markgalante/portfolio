6011e2d152ba91e2f4b7069233bfe860023127f0
Mark Galante <mark.galante.14@gmail.com>
2019-07-31 18:22:09 +0200

    Added classNames to About.js

--- myportfolio/src/Components/About.js
+++ myportfolio/src/Components/About.js
@@ -3,12 +3,12 @@ import React from 'react';
 const About = props =>{
     return(
         <section>
-            <h2>About</h2>
+            <h2 className="text-uppercase"><i className="fa fa-lg fa-user"></i>About</h2>
             <div>
                 {props.aboutData}
             </div>
         </section>
-    )
+    );
 }; 
 
 export default About; 
