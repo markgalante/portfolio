66d949dbc88f76005ec6cceb02dfa3a227778433
Mark Galante <mark.galante.14@gmail.com>
2019-07-31 17:55:03 +0200

    Re-edited changes

--- myportfolio/src/Components/Profile.js
+++ myportfolio/src/Components/Profile.js
@@ -13,8 +13,8 @@ const Profile = props =>{
                 <li><i className="fa fa-lg fa-envelope"></i><a href={`mailto:${profileObj.contact.email}`}>{profileObj.contact.email}</a></li>
             </ul>
             <div className="divider"></div>
-            <p>I built this resume using <a href="https://facebook.github.io/react/">React </a>under the guidance from the code used by <a href="https://jonbloomer.com.au/">Jonathan Bloomer</a>.</p>
-            
+            <p>I built this resume using <a href="https://facebook.github.io/react/">React</a> under the guidance from the code used by <a href="https://jonbloomer.com.au/">Jonathan Bloomer</a>.</p>
+
         </div>
     )
 };
