f5c308c3ef677d53f0629a9de349b94fd4f30a10
Mark Galante <mark.galante.14@gmail.com>
2019-08-07 18:25:31 +0200

    Edited formatting

--- myportfolio/src/Components/App.js
+++ myportfolio/src/Components/App.js
@@ -19,7 +19,7 @@ const App = props => {
         
         <aside className="col-md-4">
           <div className="inner">
-          <Profile profileData={profileData}/>
+            <Profile profileData={profileData}/>
           </div>
         </aside>
          
