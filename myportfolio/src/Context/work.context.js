a08643d909934d665ecb4a2ca305731dfa150c34
markgalante <mark.galante.14@gmail.com>
2020-07-21 21:47:35 +0200

    Corrected parsing error for Moment.js

--- myportfolio/src/Context/work.context.js
+++ myportfolio/src/Context/work.context.js
@@ -5,8 +5,8 @@ const WorkContext = createContext(
         "workHistory": [
             {
                 "employer": "Physiopaedic T/A Marq Labuschagne",
-                "started": "8 January 2018",
-                "ended": "",
+                "started": "2018-01-08", //YYYY-MM-DD
+                "ended": "", //YYYY-MM-DD
                 "position": "Physiotherapist",
                 "myrole": "I have been working here since January 2018. This practice is based at Life Vincent Pallotti Hospital in Pinelands. I was based in the Orthopaedic Ward, the Robin Trust Sub-Acute Care Centre and downstairs at the practice. I saw both in- and out-patients",
                 "highlights": [],
@@ -14,8 +14,8 @@ const WorkContext = createContext(
             },
             {
                 "employer": "Western Cape Government",
-                "started": "1 January 2017",
-                "ended": "31 December 2017",
+                "started": "2017-01-01", //YYYY-MM-DD
+                "ended": "2017-12-31", //YYYY-MM-DD
                 "position": "Community Service Physiotherapist",
                 "myrole": "This was my first job after graduating as a physiotherapist. It was part of my community serive year. I headed up and managed the delivery of physiotherapy services to the Matzikama Sub-District in the Western Cape, South Africa",
                 "highlights": [],
