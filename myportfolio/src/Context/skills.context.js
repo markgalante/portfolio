c9ffa96f828e6732796f8b8ccc1a0edcaf5f3ddf
markgalante <mark.galante.14@gmail.com>
2020-07-21 23:25:03 +0200

    Prepare links for PNG images for stack reconfiguration

--- myportfolio/src/Context/skills.context.js
+++ myportfolio/src/Context/skills.context.js
@@ -5,15 +5,15 @@ const SkillsContext = createContext({
         "title": "front-end developer",
         "level": "junior",
         "keywords": [
-            "HTML",
-            "CSS",
-            "JavaScript",
-            "jQuery",
-            "Bootstrap",
-            "React",
-            "Node.js",
-            "Express.js",
-            "MongoDB"
+            "HTML", //<img src="https://img.icons8.com/color/48/000000/html-5.png"/>
+            "CSS", //<img src="https://img.icons8.com/color/48/000000/css3.png"/>
+            "JavaScript", //<img src="https://img.icons8.com/color/48/000000/javascript.png"/>
+            "jQuery", //<img src="https://img.icons8.com/ios-filled/50/000000/jquery.png"/>
+            "Bootstrap", //<img src="https://img.icons8.com/color/48/000000/bootstrap.png"/>
+            "React", //<img src="https://img.icons8.com/color/48/000000/react-native.png"/>
+            "Node.js", //<img src="https://img.icons8.com/color/48/000000/nodejs.png"/>
+            "Express.js", //<img src="https://cnblackxp.github.io/imgs/technologies-icons/express.png" />
+            "MongoDB" //<img src="https://img.icons8.com/color/48/000000/mongodb.png"/>
         ]
     }
 }); 
