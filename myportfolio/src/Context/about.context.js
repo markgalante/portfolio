971f37fe009cbfbbeb42fdb63e853e266bb1cf7b
markgalante <mark.galante.14@gmail.com>
2020-07-22 21:56:54 +0200

    Edit blurb and justify text

--- myportfolio/src/Context/about.context.js
+++ myportfolio/src/Context/about.context.js
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 
 const AboutContext = createContext(
-    `While working full-time physiotherapist, I have spent the last
+    `While working as a full-time physiotherapist, I have spent the last
     18 months developing programming skills required for full-stack web
     development. While I am passionate about healthcare, I am relishing 
     the challenges in tech and development.`
