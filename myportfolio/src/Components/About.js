d27943c6b7e31a027d8ff96b0eafa3b2ee97c7d2
markgalante <mark.galante.14@gmail.com>
2020-07-22 21:55:34 +0200

    remov about title

--- myportfolio/src/Components/About.js
+++ myportfolio/src/Components/About.js
@@ -1,12 +1,10 @@
 import React, { useContext } from 'react'; 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
 import AboutContext from '../Context/about.context';
 
 const About = () =>{
     const about = useContext(AboutContext); 
     return(
         <section>
-            <h4><FontAwesomeIcon icon={['fas', 'user']} /> About</h4>
             <div className="about">
                 {about}
             </div>
