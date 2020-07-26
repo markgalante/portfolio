52a6807c099afdfe0b17d1d87a8d91fc541c4091
markgalante <mark.galante.14@gmail.com>
2020-07-26 16:46:41 +0200

    Updates

---
+++ precache-manifest.bd6a28bbfce03bf3fcfa85d0d94a3a67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2539eb7ca23e1c93c20570ae59adac",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c28e38b89c96066c1480",
+    "url": "/portfolio/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "4e142919088ce31992b1",
+    "url": "/portfolio/static/css/main.52eedf09.chunk.css"
+  },
+  {
+    "revision": "c28e38b89c96066c1480",
+    "url": "/portfolio/static/js/2.c178f891.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/portfolio/static/js/2.c178f891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e142919088ce31992b1",
+    "url": "/portfolio/static/js/main.2050dfae.chunk.js"
+  },
+  {
+    "revision": "272c84328c4e9a9d0bda",
+    "url": "/portfolio/static/js/runtime-main.6093c233.js"
+  }
+]);
